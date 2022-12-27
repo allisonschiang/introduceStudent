1911e952bde163f4646962faa85999bd486543e6
Allison Chiang <83091926+allisonschiang@users.noreply.github.com>
2022-12-27 14:23:31 -0800

    added background color and alert if textboxes empty

--- introduceStudent/ViewController.swift
+++ introduceStudent/ViewController.swift
@@ -23,17 +23,30 @@ class ViewController: UIViewController {
           numberOfPetsLabel.text = "\(Int(sender.value))"
       }
     
+    @IBOutlet weak var colorChooser: UIColorWell!
+    
     @IBOutlet weak var introduceButton: UIButton!
     @IBAction func introduceSelfDidTapped(_ sender: UIButton){
         let year = yearSegmentControl.titleForSegment(at: yearSegmentControl.selectedSegmentIndex)
-        let introduction = "My name is \(firstNameTextbox.text!) \(lastNameTextbox.text!) and I attend \(schoolTextbox.text!). I am currently in my \(year!) year and I have \(numberOfPetsLabel.text!) pets. It is \(morePetsSwitch.isOn) that I want more pets!"
+        let color = colorChooser.selectedColor
+        view.backgroundColor = color
         
-        let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
-        let action = UIAlertAction(title:"Nice to meet you!", style: .default, handler: nil)
-        
-        alertController.addAction(action)
-        
-        present(alertController, animated:true, completion: nil)
+        if (firstNameTextbox.text=="") || (lastNameTextbox.text=="") || (schoolTextbox.text=="") {
+            let alertController = UIAlertController(title: "Oops!", message: "You are missing some information.", preferredStyle: .alert)
+            let action = UIAlertAction(title:"Got it!", style: .default, handler: nil)
+            alertController.addAction(action)
+            present(alertController, animated:true, completion: nil)
+        }
+        else{
+            let introduction = "My name is \(firstNameTextbox.text!) \(lastNameTextbox.text!) and I attend \(schoolTextbox.text!). I am currently in my \(year!) year and I have \(numberOfPetsLabel.text!) pets. It is \(morePetsSwitch.isOn) that I want more pets!"
+            
+            let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
+            let action = UIAlertAction(title:"Nice to meet you!", style: .default, handler: nil)
+            
+            alertController.addAction(action)
+            
+            present(alertController, animated:true, completion: nil)
+        }
     }
     
     @IBOutlet weak var morePetsSwitch: UISwitch!
